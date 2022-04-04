ae900c4e58b1f12391d0264f5d9b2dc622aee251
Daniel Gomes Falcão <danielgomesfalcao@gmail.com>
2022-04-03 22:40:36 -0300

    configuracoes iniciais do projeto

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Button from "../Button";
+
+class Form extends React.Component {
+    render() {
+        return (
+            <form>
+                <div>
+                    <label>
+
+                    </label>
+                    <input />
+                </div>
+                <div>
+                    <label>
+
+                    </label>
+                    <input />
+                </div>
+                <Button />
+            </form>
+        )
+    }
+}
+
+export default Form;
